46f8b8f9631215db78198c2f4bc9b54e6d9536a0
jonx1183 <jonx1183@stud.kea.dk>
2023-09-18 14:17:41 +0200

    Can pick images

--- App.js
+++ App.js
@@ -1,10 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, FlatList, Button, View, TextInput, Text, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [text, setText] = useState('')
+  const [notes, setNotes] = useState([])
+  const [editObj, setEditObj] = useState(null)
+  const [imagePath, setImagePath] = useState(null)
+
+  
+ function buttonHandler(){
+    setNotes([...notes, {key:notes.length, name:text}])   
+  }
+
+  async function launchImagePicker(){
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true
+
+    })
+    if(!result.canceled){
+      setImagePath(result.assets[0].uri)
+
+    }
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello</Text>
+      { editObj && 
+        <View>
+          <TextInput defaultValue={editObj.text} onChangeText={(txt) => setText(txt)} />
+          <Text onPress={saveUpdate}>Save</Text>
+        </View> 
+      }
+
+      <TextInput style={styles.textInput}  onChangeText={(txt) => setText(txt)} />
+      <Button title='Press Me' onPress={buttonHandler} ></Button>
+      <FlatList
+        data={notes}
+        renderItem={(note) => 
+          <View>
+            <Text>{note.item.name}</Text>
+          </View>
+      }
+      />
+      <Image style={{width:200, height:200}} source={{uri:imagePath}} />
+      <Button title ='Picke image' onPress={launchImagePicker} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +60,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop:200
   },
+  textInput:{
+    backgroundColor:'lightblue',
+    minWidth: 200
+  }
 });
